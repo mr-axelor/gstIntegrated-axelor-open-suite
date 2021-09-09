c9eca819003aea4f708b73cd15637f1eb76ce724
Madhusudhan <m.r@axelor.com>
2021-09-09 14:28:49 +0530

    created reports in gst module

--- axelor-gst/src/main/java/com/axelor/apps/gst/GstModule.java
+++ axelor-gst/src/main/java/com/axelor/apps/gst/GstModule.java
@@ -1,8 +1,13 @@
 package com.axelor.apps.gst;
 
 import com.axelor.app.AxelorModule;
+import com.axelor.apps.account.report.IReport;
+import com.axelor.apps.account.service.invoice.print.InvoicePrintServiceImpl;
 import com.axelor.apps.account.web.InvoiceLineController;
+import com.axelor.apps.base.web.ProductController;
 import com.axelor.apps.cash.management.service.InvoiceServiceManagementImpl;
+import com.axelor.apps.gst.service.GstInvoicePrintServiceImpl;
+import com.axelor.apps.gst.service.GstProductController;
 import com.axelor.apps.gst.service.InvoiceLineGstController;
 import com.axelor.apps.gst.service.InvoiceServiceGstImpl;
 
@@ -12,5 +17,7 @@ public class GstModule extends AxelorModule {
   protected void configure() {
     bind(InvoiceServiceManagementImpl.class).to(InvoiceServiceGstImpl.class);
     bind(InvoiceLineController.class).to(InvoiceLineGstController.class);
+    bind(InvoicePrintServiceImpl.class).to(GstInvoicePrintServiceImpl.class);
+    bind(ProductController.class).to(GstProductController.class);
   }
 }
