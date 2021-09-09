b83dad197a8920ddf356d05cbe1f469faa1d4804
Madhusudhan <m.r@axelor.com>
2021-09-09 10:15:43 +0530

    spotlessApply commit

--- axelor-gst/src/main/java/com/axelor/apps/gst/service/InvoiceServiceGstImpl.java
+++ axelor-gst/src/main/java/com/axelor/apps/gst/service/InvoiceServiceGstImpl.java
@@ -1,8 +1,5 @@
 package com.axelor.apps.gst.service;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import com.axelor.apps.account.db.Invoice;
 import com.axelor.apps.account.db.InvoiceLine;
 import com.axelor.apps.account.db.repo.InvoiceRepository;
@@ -18,45 +15,49 @@ import com.axelor.apps.base.service.alarm.AlarmEngineService;
 import com.axelor.apps.cash.management.service.InvoiceServiceManagementImpl;
 import com.axelor.exception.AxelorException;
 import com.google.inject.Inject;
+import java.math.BigDecimal;
+import java.util.List;
 
-public class InvoiceServiceGstImpl extends InvoiceServiceManagementImpl{
+public class InvoiceServiceGstImpl extends InvoiceServiceManagementImpl {
 
-	  @Inject
-	  public InvoiceServiceGstImpl(
-	      ValidateFactory validateFactory,
-	      VentilateFactory ventilateFactory,
-	      CancelFactory cancelFactory,
-	      AlarmEngineService<Invoice> alarmEngineService,
-	      InvoiceRepository invoiceRepo,
-	      AppAccountService appAccountService,
-	      PartnerService partnerService,
-	      InvoiceLineService invoiceLineService,
-	      AccountConfigService accountConfigService,
-	      MoveToolService moveToolService) {
-	    super(
-	        validateFactory,
-	        ventilateFactory,
-	        cancelFactory,
-	        alarmEngineService,
-	        invoiceRepo,
-	        appAccountService,
-	        partnerService,
-	        invoiceLineService,
-	        accountConfigService,
-	        moveToolService);
-	  }
-	  
-	  @Override
-	  public Invoice compute(final Invoice invoice) throws AxelorException {
-		  Invoice computedInvoice = super.compute(invoice);
-		  List<InvoiceLine> invoiceLineList = computedInvoice.getInvoiceLineList();
-		  BigDecimal igst = invoiceLineList.stream().map(i -> i.getIgst()).reduce(BigDecimal.ZERO, BigDecimal::add);
-		  BigDecimal cgst = invoiceLineList.stream().map(i -> i.getCgst()).reduce(BigDecimal.ZERO, BigDecimal::add);
-		  BigDecimal sgst = invoiceLineList.stream().map(i -> i.getSgst()).reduce(BigDecimal.ZERO, BigDecimal::add);
-		  computedInvoice.setNetCgst(cgst);
-		  computedInvoice.setNetSgst(sgst);
-		  computedInvoice.setNetIgst(igst);
-		  return computedInvoice;
-	  }
+  @Inject
+  public InvoiceServiceGstImpl(
+      ValidateFactory validateFactory,
+      VentilateFactory ventilateFactory,
+      CancelFactory cancelFactory,
+      AlarmEngineService<Invoice> alarmEngineService,
+      InvoiceRepository invoiceRepo,
+      AppAccountService appAccountService,
+      PartnerService partnerService,
+      InvoiceLineService invoiceLineService,
+      AccountConfigService accountConfigService,
+      MoveToolService moveToolService) {
+    super(
+        validateFactory,
+        ventilateFactory,
+        cancelFactory,
+        alarmEngineService,
+        invoiceRepo,
+        appAccountService,
+        partnerService,
+        invoiceLineService,
+        accountConfigService,
+        moveToolService);
+  }
 
+  @Override
+  public Invoice compute(final Invoice invoice) throws AxelorException {
+    Invoice computedInvoice = super.compute(invoice);
+    List<InvoiceLine> invoiceLineList = computedInvoice.getInvoiceLineList();
+    BigDecimal igst =
+        invoiceLineList.stream().map(i -> i.getIgst()).reduce(BigDecimal.ZERO, BigDecimal::add);
+    BigDecimal cgst =
+        invoiceLineList.stream().map(i -> i.getCgst()).reduce(BigDecimal.ZERO, BigDecimal::add);
+    BigDecimal sgst =
+        invoiceLineList.stream().map(i -> i.getSgst()).reduce(BigDecimal.ZERO, BigDecimal::add);
+    computedInvoice.setNetCgst(cgst);
+    computedInvoice.setNetSgst(sgst);
+    computedInvoice.setNetIgst(igst);
+    return computedInvoice;
+  }
 }
