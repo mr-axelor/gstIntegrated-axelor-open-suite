d66f9c18ab830ecfdca92ad3b982cd5c04227626
Madhusudhan <m.r@axelor.com>
2021-09-09 10:14:13 +0530

    completed reports and other changes

--- axelor-gst/src/main/java/com/axelor/apps/gst/GstModule.java
+++ axelor-gst/src/main/java/com/axelor/apps/gst/GstModule.java
@@ -1,17 +1,16 @@
 package com.axelor.apps.gst;
 
 import com.axelor.app.AxelorModule;
-import com.axelor.apps.account.service.invoice.generator.InvoiceGenerator;
 import com.axelor.apps.account.web.InvoiceLineController;
 import com.axelor.apps.cash.management.service.InvoiceServiceManagementImpl;
 import com.axelor.apps.gst.service.InvoiceLineGstController;
 import com.axelor.apps.gst.service.InvoiceServiceGstImpl;
 
 public class GstModule extends AxelorModule {
-	
-	  @Override
-	  protected void configure() {
-		  bind(InvoiceServiceManagementImpl.class).to(InvoiceServiceGstImpl.class);
-		  bind(InvoiceLineController.class).to(InvoiceLineGstController.class);
-	  }
+
+  @Override
+  protected void configure() {
+    bind(InvoiceServiceManagementImpl.class).to(InvoiceServiceGstImpl.class);
+    bind(InvoiceLineController.class).to(InvoiceLineGstController.class);
+  }
 }
--- axelor-gst/src/main/java/com/axelor/apps/gst/service/InvoiceLineGstController.java
+++ axelor-gst/src/main/java/com/axelor/apps/gst/service/InvoiceLineGstController.java
@@ -1,7 +1,5 @@
 package com.axelor.apps.gst.service;
 
-import java.math.BigDecimal;
-
 import com.axelor.apps.account.db.Invoice;
 import com.axelor.apps.account.db.InvoiceLine;
 import com.axelor.apps.account.web.InvoiceLineController;
@@ -9,34 +7,47 @@ import com.axelor.exception.AxelorException;
 import com.axelor.i18n.I18n;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import java.math.BigDecimal;
 
 public class InvoiceLineGstController extends InvoiceLineController {
-	@Override
-	public void compute(ActionRequest request, ActionResponse response) throws AxelorException {
-		super.compute(request, response);
-		Invoice invoice = request.getContext().getParent().asType(Invoice.class);
-		InvoiceLine invoiceLine = request.getContext().asType(InvoiceLine.class);
-		response.setValue("cgst", 0);
-		response.setValue("sgst", 0);
-		response.setValue("igst", 0);
-		if(invoiceLine.getTaxLine()!=null && invoiceLine.getTaxLine().getTax().getCode().contains("GST")) {
-			BigDecimal gstRate = invoiceLine.getTaxLine().getValue();
-			BigDecimal qty = invoiceLine.getQty();
-			BigDecimal unitPrice = invoiceLine.getPrice();
-			BigDecimal totalPriceWithoutTax = qty.multiply(unitPrice);
-			BigDecimal totalGstTax = gstRate.multiply(totalPriceWithoutTax);
-			if(invoice.getAddress()!=null && invoice.getCompany().getAddress()!=null && invoice.getAddress().getState()!=null && invoice.getCompany().getAddress().getState()!=null){
-				if(!(invoice.getAddress().getState()!=null && invoice.getAddress().getState().equals(invoice.getCompany().getAddress().getState()))) {
-					response.setValue("igst", totalGstTax);
-					
-				}else {
-					response.setValue("cgst", totalGstTax.divide(BigDecimal.valueOf(2)));
-					response.setValue("sgst", totalGstTax.divide(BigDecimal.valueOf(2)));
-				}
-			}else {
-				response.setValue("taxLine",null);
-				response.setFlash(I18n.get("Both the invoice address & company address should contain STATE information without which it is not possible to calculate the GST"));
-			}
-		}	
-	}
+  @Override
+  public void compute(ActionRequest request, ActionResponse response) throws AxelorException {
+    super.compute(request, response);
+    Invoice invoice = request.getContext().getParent().asType(Invoice.class);
+    InvoiceLine invoiceLine = request.getContext().asType(InvoiceLine.class);
+    response.setValue("cgst", 0);
+    response.setValue("sgst", 0);
+    response.setValue("igst", 0);
+    if (invoiceLine.getTaxLine() != null
+        && invoiceLine.getTaxLine().getTax().getCode().contains("GST")) {
+      BigDecimal gstRate = invoiceLine.getTaxLine().getValue();
+      BigDecimal qty = invoiceLine.getQty();
+      BigDecimal unitPrice = invoiceLine.getPrice();
+      BigDecimal totalPriceWithoutTax = qty.multiply(unitPrice);
+      BigDecimal totalGstTax = gstRate.multiply(totalPriceWithoutTax);
+      if (invoice.getAddress() != null
+          && invoice.getCompany().getAddress() != null
+          && invoice.getAddress().getState() != null
+          && invoice.getCompany().getAddress().getState() != null) {
+        if (!(invoice.getAddress().getState() != null
+            && invoice
+                .getAddress()
+                .getState()
+                .equals(invoice.getCompany().getAddress().getState()))) {
+          response.setValue("igst", totalGstTax);
+
+        } else {
+          response.setValue("cgst", totalGstTax.divide(BigDecimal.valueOf(2)));
+          response.setValue("sgst", totalGstTax.divide(BigDecimal.valueOf(2)));
+        }
+      } else {
+        response.setValue("taxLine", null);
+        response.setValue("inTaxTotal", 0);
+        // response.setError(I18n.get("cant"));
+        response.setFlash(
+            I18n.get(
+                "Both the invoice address & company address should contain STATE information without which it is not possible to calculate the GST"));
+      }
+    }
+  }
 }
